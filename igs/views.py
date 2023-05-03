ed895c0ff0ed92c29b9738cb14c5a13d088c9455
lukvasquez <lukas.vasquez.verdejo@gmail.com>
2023-05-02 21:00:39 -0400

    refactor: refactoring  default index view to development

--- igs/views.py
+++ igs/views.py
@@ -6,10 +6,9 @@ def directions(request):
     html = """<h3>Urls</h3>
 
             <p><b>Provisionalmente</b>, cada template ubicado 
-            en web_design/templates, se pude visualizar agregando "show" a url :<br>
-              default_url/show/example.html/</p>
+            en web_design/templates, se pude visualizar agregando "show" a url: </p>
               <p>Por ejemplo:<br>
-              http://127.0.0.1:8000//show/base.html
+              http://127.0.0.1:8000/show/base.html
               </p>
 
               """
