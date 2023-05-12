14f429a59a1c61e2f374949c3c51f1d19bfbada5
Nahuel <nahuelmath@gmail.com>
2023-05-12 00:18:08 -0400

    add: register_user view, need replace for templates urls

--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+def register_user(request):
+    if request.method == 'GET':
+        render(request, "direction")
+    
+    elif request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = User.objects.create_user(username=username,
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        email=email,
+                                        password=password)
+        
+        return HttpResponseRedirect("/account_status")
