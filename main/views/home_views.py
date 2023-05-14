9d9c5a8a9a6df15e25094078d730029763ccfdab
lukvasquez <lukas.vasquez.verdejo@gmail.com>
2023-05-14 00:16:03 -0400

    refactor: refactoring get_transaction in  home views  using Django native tools

--- main/views/home_views.py
+++ main/views/home_views.py
@@ -3,31 +3,25 @@ from django.template import Template,Context,loader
 from django.shortcuts import render
 from main.models import Incomes,Outcomes,AccountStatus
 from django.contrib.auth.models import User
-from django.db.models import F
+from django.db.models import F,Value,CharField
 ## views home 
 
 def get_transactions(request,type):
     #user_id=request.user.id
     user_id=4
     dict_data={}
-    print(type)
     if(type=="Ingreso"):
-        incomes=Incomes.objects.filter(account_status__user=user_id)[:10]
+        incomes=Incomes.objects.filter(account_status__user=user_id).order_by(F("set_at").desc())
+        incomes=incomes.annotate(type=Value('Ingreso', output_field=CharField())).annotate(amount=F('income'))[:10]        
         dict_data=list(incomes.values())
-        for values in dict_data:
-            values["monto"]=values.pop("income")
-            values["tipo"]="Ingreso"
-
-
+                   
         return JsonResponse(dict_data,safe=False)
         
     if(type=="Egreso"):
-        outcomes=Outcomes.objects.filter(account_status__user=user_id)[:10]
+        outcomes=Outcomes.objects.filter(account_status__user=user_id).order_by(F("set_at").desc())
+        outcomes=outcomes.annotate(type=Value('Egreso', output_field=CharField())).annotate(amount=F('outcome'))[:10]        
         dict_data=list(outcomes.values())
-        for values in dict_data:
-            values["monto"]=values.pop("outcome")
-            values["tipo"]="Egreso"
-
+        
         return JsonResponse(dict_data,safe=False)
 
     return JsonResponse(dict_data)
@@ -36,17 +30,8 @@ def get_transactions(request,type):
 def home(request):
     template=loader.get_template("home.html")
     #user_id=request.user.id
-    user_id=4
-    #outcomes=Outcomes.objects.filter(account_status__user=user_id)
-    incomes=Incomes.objects.filter(account_status__user=user_id).order_by(F("set_at").desc())[:20]
-    #union=outcomes.union(incomes).order_by(F("set_at").desc())[:20]
-
-    #data=get_transactions(HttpRequest,"Ingreso")
-    
-    # Test
-    #transactions=union
-    
-    ctx={"transactions":incomes}
+    #ctx={"Amount":}
+    ctx={}
     rendered_template=template.render(ctx)
     return HttpResponse(rendered_template)
 
