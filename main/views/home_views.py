fcfcd69c53761dc141d8e8204bcc23ac5fd299b9
lukvasquez <lukas.vasquez.verdejo@gmail.com>
2023-05-14 01:13:21 -0400

    feat:table filters uploaded now works for All (outcomes and incomes)

--- main/views/home_views.py
+++ main/views/home_views.py
@@ -10,18 +10,27 @@ def get_transactions(request,type):
     #user_id=request.user.id
     user_id=4
     dict_data={}
-    if(type=="Ingreso"):
+    if(type=="Income"):
         incomes=Incomes.objects.filter(account_status__user=user_id).order_by(F("set_at").desc())
         incomes=incomes.annotate(type=Value('Ingreso', output_field=CharField())).annotate(amount=F('income'))[:10]        
         dict_data=list(incomes.values())
                    
         return JsonResponse(dict_data,safe=False)
         
-    if(type=="Egreso"):
+    if(type=="Outcome"):
         outcomes=Outcomes.objects.filter(account_status__user=user_id).order_by(F("set_at").desc())
         outcomes=outcomes.annotate(type=Value('Egreso', output_field=CharField())).annotate(amount=F('outcome'))[:10]        
         dict_data=list(outcomes.values())
-        
+
+        return JsonResponse(dict_data,safe=False)
+
+    if(type=="All"):
+        outcomes=Outcomes.objects.filter(account_status__user=user_id).annotate(type=Value('Egreso', output_field=CharField())).annotate(amount=F('outcome'))        
+        outcomes=outcomes.values("id","amount", "category",  "set_at", "type" )        
+        incomes=Incomes.objects.filter(account_status__user=user_id).annotate(type=Value('Ingreso', output_field=CharField())).annotate(amount=F('income'))        
+        incomes=incomes.values("id","amount", "category", "set_at", "type" )       
+        union_table=outcomes.union(incomes).order_by(F("set_at").desc())[:10]
+        dict_data=list(union_table)
         return JsonResponse(dict_data,safe=False)
 
     return JsonResponse(dict_data)
