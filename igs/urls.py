57c4a3a4a9dc62ba07a27ef370d18392f2fdd92e
RenzoZanca <renzozanca@gmail.com>
2023-05-14 22:12:45 -0400

    fix esthetic register

--- igs/urls.py
+++ igs/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from  main.views import home_views,log_in_views,transaction_views, registro_views
+from  main.views import home_views,log_in_views,transaction_views, registro_views, register_view
 
 urlpatterns = [
     path("", log_in_views.log_in),
@@ -26,6 +26,7 @@ urlpatterns = [
     path('transaction/', transaction_views.transaction,name="transaction"),
     path("", include("main.urls")),
     path('registro/', registro_views.registro,name="registro"),
+    path("register/", register_view.register_user, name="register"),
 ]
 
 
