de7bbd38d81b2cf09b25bcbf8769bc3953c90e1a
Nahuel <nahuelmath@gmail.com>
2023-05-14 22:40:35 -0400

    add: register models for admin site

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import AccountStatus, Incomes, Outcomes
+
+admin.site.register(AccountStatus)
+admin.site.register(Incomes)
+admin.site.register(Outcomes)
