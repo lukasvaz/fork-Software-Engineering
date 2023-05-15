5ce4db1cc583129d493c1d1f6e7b0a2e82f67d8c
Nahuel <nahuelmath@gmail.com>
2023-05-14 22:41:30 -0400

    feat: command to create dummy data

---
+++ main/management/commands/set_up_dummy_data.py
@@ -0,0 +1,43 @@
+import random
+from typing import Any, Optional
+
+from django.db import transaction
+from django.core.management.base import BaseCommand
+
+from main.models import AccountStatus
+from main.factories import UserFactory, AccountStatusFactory, IncomesFactory, OutcomesFactory
+from django.contrib.auth.models import User
+
+NUM_USERS = 10
+NUM_ACC_STATUSES = 10
+NUM_IN_OUT = 10
+
+class Command(BaseCommand):
+    help = "Generate dummy data"
+
+    @transaction.atomic
+    def handle(self, *args: Any, **options: Any):
+        self.stdout.write("Deleting old data...")
+        models = [User, AccountStatus]
+        for m in models:
+            m.objects.all().delete()
+
+        self.stdout.write("Creating new data...")
+        users = []
+        account_statuses = []
+        
+        for _ in range(NUM_USERS):
+            user = UserFactory()
+            users.append(user)
+        
+        for _ in range(NUM_ACC_STATUSES):
+            user = random.choice(users)
+            account_status = AccountStatusFactory(user=user)
+            users.remove(user)
+            account_statuses.append(account_status)
+        
+        for _ in range(NUM_IN_OUT):
+            account_status = random.choice(account_statuses)
+            income = IncomesFactory(account_status=account_status)
+            outcome = OutcomesFactory(account_status=account_status)
+            account_statuses.remove(account_status)
