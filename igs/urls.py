60a7a2c74f67e81ebfeda083ab8ec02f4a38c551
lukvasquez <lukas.vasquez.verdejo@gmail.com>
2023-05-16 13:25:58 -0400

    refactor: deleting unused log_in views

--- igs/urls.py
+++ igs/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from  main.views import home_views,log_in_views,transaction_views, register_view
+from  main.views import home_views,transaction_views, register_view
 
 urlpatterns = [
     path('',  lambda req:redirect('accounts/login'), name='root'),
