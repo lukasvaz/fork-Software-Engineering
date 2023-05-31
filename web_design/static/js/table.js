ebb3cd6bbf3fef62d42535180fe10c4650057682
lukvasquez <lukas.vasquez.verdejo@gmail.com>
2023-05-31 17:27:17 -0400

    refactor(home):refactoring url request and create column in home js  to make it  more readable

--- web_design/static/js/table.js
+++ web_design/static/js/table.js
@@ -1,12 +1,12 @@
     
     /* this function get the selected value in transactio_type option field and send an ajax request  to the database 
         and  rendering the Json response into the table in an  asychronus way */
+       
         async function updateTable(e) {
             const selectType = document.getElementById('transaction_type');
         var selectedType = selectType.value;
         const xhttp = new XMLHttpRequest();
-        var url = "get-table/All".replace(/0/, selectedType.toString());
-        
+        var url = "get-table/"+selectedType;        
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 const response = this.response;
@@ -16,22 +16,16 @@
                 
                 for(var i in json_response){
                     var transaction=json_response[i];
-                    var row=document.createElement('tr');                
-                    createColumn(row,transaction,'set_at','th')
-                    createColumn(row,transaction,'description','td')
-                    createColumn(row,transaction,'amount','td')
-                    createColumn(row,transaction,'category','td')
-                    createColumn(row,transaction,'type','td')
-                    table.appendChild(row);
+                    table.innerHTML+=`<tr>\
+                    <th>${transaction['set_at']}</th>\
+                    <th>${transaction['description']}</th>\
+                    <th>${transaction['amount']}</th>\
+                    <th>${transaction['category']}</th>\
+                    <th>${transaction['type']}</th>\
+                    `
                 }
           }
         }
         xhttp.open("GET", url);
         xhttp.send();
     }
-    /* this function get a  row and add columns nodes to it */
-    function createColumn(row,transaction,atr,type){
-        var column=document.createElement(type);
-            column.innerText=transaction[atr];
-            row.appendChild(column)
-        }
