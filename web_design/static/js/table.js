4dd7d3e8f6ce255cbf33c724fb74bde7a25dfef7
lukvasquez <lukas.vasquez.verdejo@gmail.com>
2023-06-04 22:38:58 -0400

    feat(home):sorting table by date(desc)

--- web_design/static/js/table.js
+++ web_design/static/js/table.js
@@ -9,18 +9,28 @@
         tableBody.innerHTML='';
 
         for(var i in data){
-                            var transaction=data[i];
-                            tableBody.innerHTML+=`<tr>\
-                            <th>${transaction['set_at']}</th>\
-                            <td>${transaction['description']}</td>\
-                            <td>${transaction['amount']}</td>\
-                            <td>${transaction['category']}</td>\
-                            <td>${transaction['type']}</td>
-                            <td><i class="fa-solid fa-pen"></i> <i class="fa-sharp fa-solid fa-trash"></i></td></tr>`
-                            /*ultima  columna  es para  para borrar y modificar, si quieren acceder al id  de la transaccion  deben pedir transaction['id'], 
-                            como  está  todo en una tabla  se debe recuperar si  es  Ingreso o Egreso */
-                            console.log(transaction['id'])
+            var transaction=data[i];
+            tableBody.innerHTML+=`<tr>\
+            <th>${transaction['set_at']}</th>\
+            <td>${transaction['description']}</td>\
+            <td>${transaction['amount']}</td>\
+            <td>${transaction['category']}</td>\
+            <td>${transaction['type']}</td>
+            <td><i class="fa-solid fa-pen"></i> <i class="fa-sharp fa-solid fa-trash"></i></td></tr>`
+            /*ultima  columna  es para  para borrar y modificar, si quieren acceder al id  de la transaccion  deben pedir transaction['id'], 
+            como  está  todo en una tabla  se debe recuperar si  es  Ingreso o Egreso */
+            console.log(transaction['id'])
                         }    
             
-        $('#home-table').DataTable();}
+        var table=$('#home-table').DataTable({
+            paging:true,
+            searching:false,
+            ordering:false,
+            info:false,
+            searching:false,
+            autowidth:true,
+            lengthChange:false,
+        });
+        table.column('0').order('desc').draw()
+    }
     
