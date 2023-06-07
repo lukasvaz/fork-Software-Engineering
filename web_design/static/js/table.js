6eb8fad3502dd71bec4c60df028bf50c2c93a064
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-06-06 21:16:50 -0400

    fix: indentation and redirect for edit button

--- web_design/static/js/table.js
+++ web_design/static/js/table.js
@@ -1,26 +1,27 @@
     
-    async function updateTable(e) {
-        const selectType = document.getElementById('transaction_type');
-        var selectedType = selectType.value;
-        const response = await fetch(`get-table/${selectedType}`)
-        const data = await response.json();
-        console.log(data)
-        var tableBody=document.getElementById('home-table-body')
-        tableBody.innerHTML='';
+async function updateTable(e) {
+    const selectType = document.getElementById('transaction_type');
+    var selectedType = selectType.value;
+    const response = await fetch(`get-table/${selectedType}`)
+    const data = await response.json();
+    console.log(data)
+    var tableBody=document.getElementById('home-table-body')
+    tableBody.innerHTML=``;
 
-        for(var i in data){
-                            var transaction=data[i];
-                            tableBody.innerHTML+=`<tr>\
-                            <th>${transaction['set_at']}</th>\
-                            <td>${transaction['description']}</td>\
-                            <td>${transaction['amount']}</td>\
-                            <td>${transaction['category']}</td>\
-                            <td>${transaction['type']}</td>
-                            <td><i class="fa-solid fa-pen"></i> <i class="fa-sharp fa-solid fa-trash"></i></td></tr>`
-                            /*ultima  columna  es para  para borrar y modificar, si quieren acceder al id  de la transaccion  deben pedir transaction['id'], 
-                            como  está  todo en una tabla  se debe recuperar si  es  Ingreso o Egreso */
-                            console.log(transaction['id'])
-                        }    
-            
-        $('#home-table').DataTable();}
+    for(var i in data){
+                        var transaction=data[i];
+                        tableBody.innerHTML+=`<tr>\
+                        <th>${transaction['set_at']}</th>\
+                        <td>${transaction['description']}</td>\
+                        <td>${transaction['amount']}</td>\
+                        <td>${transaction['category']}</td>\
+                        <td>${transaction['type']}</td>
+                        <td><a href="/modify"><i class="fa-solid fa-pen" id="edit_button"></i></a><i class="fa-sharp fa-solid fa-trash"></i></td></tr>`
+                        /*ultima  columna  es para  para borrar y modificar, si quieren acceder al id  de la transaccion  deben pedir transaction['id'], 
+                        como  está  todo en una tabla  se debe recuperar si  es  Ingreso o Egreso */
+                        console.log(transaction['id'])
+                    }    
+        
+    $('#home-table').DataTable();
+}
     
