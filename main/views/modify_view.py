111ddc25f90d7f5f2c59afa0084ca538f1feda2e
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-06-07 19:08:40 -0400

    feat: save entries in view for income and outcome

--- main/views/modify_view.py
+++ main/views/modify_view.py
@@ -1,26 +1,46 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from django.contrib.auth.models import User
-from main.models import AccountStatus
+from main.models import Incomes, Outcomes
 
 
 def modify_income(request: HttpRequest, id):
-    """Modify the Income/Outcome entry by the new parameters given in the request.
+    """Modify the Income's fields by the new parameters given in the request form, uses the `id`
+    parameter passed in the url to get the specific income.
     """
 
     if request.method == 'GET':
         return render(request, "modify_entry.html")
 
     elif request.method == 'POST':
-        # account_status = sacar por id
-        user_id = request.user.id
-        amount = request.POST['monto']
-        category = request.POST['categoria']
-        set_at = request.POST['fecha']
-        description = request.POST['descripcion']
-        account_status = AccountStatus.objects.get(pk=id)
+        income_entry = Incomes.objects.get(pk=id)
+
+        income_entry.income = request.POST['monto']
+        income_entry.category = request.POST['categoria']
+        income_entry.set_at = request.POST['fecha']
+        income_entry.description = request.POST['descripcion']
+
+        income_entry.save()
 
         return redirect("/home/")
 
+
 def modify_outcome(request: HttpRequest, id):
-    ...
+    """Modify the Outcome's fields by the new parameters given in the request form, uses
+    the `id` parameter passed by the url to get the specific outcome.
+    """
+
+    if request.method == 'GET':
+        return render(request, "modify_entry.html")
+
+    elif request.method == 'POST':
+        outcome_entry = Outcomes.objects.get(pk=id)
+
+        outcome_entry.outcome = request.POST['monto']
+        outcome_entry.category = request.POST['categoria']
+        outcome_entry.set_at = request.POST['fecha']
+        outcome_entry.description = request.POST['descripcion']
+
+        outcome_entry.save()
+
+        return redirect("/home")
