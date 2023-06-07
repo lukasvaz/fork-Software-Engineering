2809cce38f4dae7ac5e4a579a3de83be826c0022
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-06-07 19:31:32 -0400

    add: doc to registration view

--- main/views/register_view.py
+++ main/views/register_view.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from main.models import AccountStatus
+from django.http import HttpRequest
 
-##view for the register form , if request is GET method render the template  
-#if is POST save user in database
-def register_user(request):
+
+def register_user(request: HttpRequest):
+    """"(`POST`) Save the registration user information in the database and redirects to the login page.
+    (`GET`) Render the registration form template.
+    """
+
     if request.method == 'GET':
         return render(request, "register.html")
-    
+
     elif request.method == 'POST':
         username = request.POST['username']
         first_name = request.POST['first_name']
@@ -16,12 +20,12 @@ def register_user(request):
         password = request.POST['password']
 
         user = User.objects.create_user(username=username,
-                                 first_name=first_name,
-                                 last_name=last_name,
-                                 email=email,
-                                 password=password)
-        
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        email=email,
+                                        password=password)
+
         user_account_status = AccountStatus(user=user)
         user_account_status.save()
-        
+
         return redirect("/accounts/login/")
