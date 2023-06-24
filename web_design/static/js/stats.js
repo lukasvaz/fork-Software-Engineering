dd49a3ccfa873bc573100f763a5b369af24d54fa
lukvasquez <lukas.vasquez.verdejo@gmail.com>
2023-06-23 22:20:18 -0400

    feat(stats):first functional chart of categories

--- web_design/static/js/stats.js
+++ web_design/static/js/stats.js
@@ -1,27 +1,36 @@
+/* Encapsulates charts renderin in functions */
+
+async function loadChart1(){
+/*  Incomes Outcomes chart */
 const response = await fetch('../get-table');
 const data = await response.json();
+const seriesData = [];
+data.forEach(element => {
+  seriesData.push({
+    name:element.category,
+    value:element.amount,
+  })
+});
+console.log(seriesData);
+// Initialize the echarts instance based on the prepared dom
+ var myChart = echarts.init(document.getElementById('main'));
+// Specify the configuration items and data for the chart
+var option = {
+  title: {
+    text: 'ECharts Getting Started Example'
+  },
+  series: [
+    {
+      type: 'pie',
+      radius: ['40%', '70%'],
+      data: seriesData,
+    }
+  ],
+};
+  
+
+// Display the chart using the configuration items and data just specified.
+  myChart.setOption(option);
+}
+
     
-$.ajax({url:'../get-table'}).done(function (response){
-        console.log(typeof(response))
-        // Initialize the echarts instance based on the prepared dom
-        var data = JSON.parse(response);
-        var myChart = echarts.init(document.getElementById('main'));
-  
-        // Specify the configuration items and data for the chart
-        var option = {
-          title: {
-            text: 'ECharts Getting Started Example'
-          },
-          series: [
-            {
-              type: 'pie',
-              data: data.amount
-            }
-          ]
-        };
-  
-        // Display the chart using the configuration items and data just specified.
-        myChart.setOption(option);
-      
-      })
-        
