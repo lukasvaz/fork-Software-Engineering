81f4771353b4a59d474468bceca75e461a5cfc4f
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-06-25 15:54:00 -0400

    fix: according module utilities

--- web_design/static/js/modify_entry.js
+++ web_design/static/js/modify_entry.js
@@ -16,17 +16,3 @@ function checksInputs(event) {
         setSuccessFor(amount);
     }
 };
-
-function setErrorFor(input, message, event) {
-    event.preventDefault();
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
-
-    small.innerText = message;
-    formControl.className = "form-control error";
-};
-
-function setSuccessFor(input) {
-    const formControl = input.parentElement;
-    formControl.className = "form-control success";
-};
