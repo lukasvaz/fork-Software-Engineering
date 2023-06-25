52daa7768505098d74a8459e9491379393da9490
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-06-25 16:16:07 -0400

    add: validations transaction form

--- web_design/static/js/transacciones.js
+++ web_design/static/js/transacciones.js
@@ -1,46 +1,45 @@
-const formulario = document.querySelector('form');
-const tipo = document.getElementById('id_tipo');
-const monto = document.getElementById('id_monto');
-const fecha = document.getElementById('id_fecha');
-const categoria = document.getElementById('id_categoria');
+const form = document.getElementById("transaction-form");
+const amount = document.getElementById("id_monto");
+const date = document.getElementById("id_fecha");
+const category = document.getElementById("id_categoria");
 
-formulario.addEventListener('submit', function(event) {
-  
-  // Validación del campo "Tipo"
-  if (tipo.value === '') {
-    alert('Debe seleccionar un tipo de ingreso o gasto');
-    return;
-  }
-
-  // Validación del campo "Monto"
-  if (monto.value === '' || isNaN(monto.value)) {
-    alert('Debe ingresar un monto válido');
-    return;
-  }
-
-  // Validación del campo "Fecha"
-  if (fecha.value === '') {
-    alert('Debe ingresar una fecha');
-    return;
-  }
-
-  // Validación del campo "Categoría"
-  if (categoria.value === '') {
-    alert('Debe seleccionar una categoría');
-    return;
-  }
-
+form.addEventListener('submit', function(event) {  
+  checksInputs(event);
 });
 
-// agregar puntos al monto:
+function checksInputs(event) {
+    const amountValue = amount.value.trim();
+    const dateValue = date.value;
+    const categoryValue = category.value;
+
+    if (amountValue === "") {
+    setErrorFor(amount, "El monto no puede estar en blanco.", event);
+    } else if (isNaN(Number(amountValue))) {
+    setErrorFor(amount, "Ingrese un monto valido.", event);
+    } else {
+    setSuccessFor(amount);
+    }
+
+    if (dateValue === "") {
+        setErrorFor(date, "Seleccione una fecha.", event);
+    } else {
+        setSuccessFor(date);
+    }
+
+    if (categoryValue === "") {
+        setErrorFor(category, "Seleccione una categoria.", event);
+    } else {
+        setSuccessFor(category);
+    }
+};
 
 // Obtenemos el campo de entrada utilizando su ID
-const montoInput = document.getElementById('monto');
+// const montoInput = document.getElementById('monto');
 
-// Agregamos un evento al campo de entrada que se activará mientras el usuario escribe
-montoInput.addEventListener('input', function() {
-  // Obtenemos el valor actual del campo de entrada
-  let valor = montoInput.value.replace(/\./g, '');
-  // Removemos cualquier punto que ya haya sido agregado previamente
-  montoInput.value = valor.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-});
+// // Agregamos un evento al campo de entrada que se activará mientras el usuario escribe
+// montoInput.addEventListener('input', function() {
+//   // Obtenemos el valor actual del campo de entrada
+//   let valor = montoInput.value.replace(/\./g, '');
+//   // Removemos cualquier punto que ya haya sido agregado previamente
+//   montoInput.value = valor.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+// });
