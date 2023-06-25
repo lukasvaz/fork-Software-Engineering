e30165dfd7197e12a55dfb57941443f0eb59a693
lukvasquez <lukas.vasquez.verdejo@gmail.com>
2023-06-24 20:36:02 -0400

    feat(stats):Adding second chart, showing savings over  time

--- web_design/static/js/stats.js
+++ web_design/static/js/stats.js
@@ -11,13 +11,15 @@ data.forEach(element => {
     value:element.amount,
   })
 });
-console.log(seriesData);
 // Initialize the echarts instance based on the prepared dom
- var myChart = echarts.init(document.getElementById('main'));
+ var myChart = echarts.init(document.getElementById('pie-plot'));
 // Specify the configuration items and data for the chart
 var option = {
   title: {
     text: 'ECharts Getting Started Example'
+  },
+  tooltip: {
+    trigger: 'item'
   },
   series: [
     {
@@ -33,4 +35,50 @@ var option = {
   myChart.setOption(option);
 }
 
+
+
+async function loadChart2(){
+  /*  Incomes Outcomes chart */
+  const response = await fetch('../get-table');
+  const data = await response.json();
+ 
+  accountStatuses=[]
+  actualStatus=0
+  dates=[]
+  /* preprocesing data */
+  data.reverse().forEach(element => { 
+    if(element.type=='Egreso'){element.amount= -element.amount};
+    actualStatus+=element.amount;
+    accountStatuses.push(actualStatus);
+    dates.push(element.set_at)
+  });
+  // Initialize the echarts instance based on the prepared dom
+   var myChart = echarts.init(document.getElementById('line-plot'));
+
+  // Specify the configuration items and data for the chart
+  option = {
+    title: {
+      text: 'Ahorros'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    xAxis: {
+      data: dates,
+    },
+    yAxis: {min:'dataMin',
+          max:'dataMax'
+  },
+    series: [
+      {
+        name:'Total ahorrado',
+        data: accountStatuses,
+        type: 'line',
+        areaStyle: {}
+      },
+    ]
+  };
+  // Display the chart using the configuration items and data just specified.
+    myChart.setOption(option);
+  }
     
