c7a0591d43550ff712d7b81147abbc96eca2413d
Nahuel <nahuelmath@gmail.com>
2023-06-26 01:59:32 -0400

    add: modal helper control

---
+++ web_design/static/js/modal.js
@@ -0,0 +1,11 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const modal_container = document.getElementById("modal_container");
+
+open.addEventListener("click", () => {
+    modal_container.classList.add("show");
+})
+
+close.addEventListener("click", () => {
+    modal_container.classList.remove("close");
+})
