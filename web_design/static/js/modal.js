a2bc389014f6ae3cf27e3522821a1baec478cf8a
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-07-01 16:24:31 -0400

    fix: change id element according descriptive name

--- web_design/static/js/modal.js
+++ web_design/static/js/modal.js
@@ -1,6 +1,6 @@
-const open = document.getElementById("open");
-const close = document.getElementById("close");
-const modal_container = document.getElementById("modal_container");
+const open = document.getElementById("open-tutorial");
+const close = document.getElementById("close-tutorial");
+const modal_container = document.getElementById("popup-container");
 
 open.addEventListener("click", () => {
     modal_container.classList.add("show");
