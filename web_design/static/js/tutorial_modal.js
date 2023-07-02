8b482b1ac9bf847feb9cd560c1145e7984f5d49b
Nahuel <nahuelmath@gmail.com>
2023-07-02 03:52:15 -0400

    add: modal tutorial

--- web_design/static/js/tutorial_modal.js
+++ web_design/static/js/tutorial_modal.js
@@ -1,8 +1,17 @@
-$(".show-modal-tutorial").click(function() {
-    const tutorialIntroHtmlContent = `<p>pene</p>`;
+$(document).ready(function() {
+    $(".show-modal-tutorial").click(function() {
+        const tutorialIntroHtmlContent = `
+        <div id="container-modal-tutorial-icon">
+            <p style="font-size: 18px">Hola bienvenido a Financhef, acá podrás encontrar ayuda para utilizar
+            la aplicacion web número uno de Beauchef:</p>
+            <a class="show-modal-tutorial-secondary" style="cursor: pointer; color: #b91a1d;font-size: 20px; font-weight: 700;">Enseñame!</a>
+        </div>`;
+    
+        $("#tutorial-modal").html(tutorialIntroHtmlContent);
+        $("#tutorial-modal").modal({
+            fadeDuration: 200
+        });
 
-    $("#tutorial-modal").html(tutorialIntroHtmlContent);
-    $("#tutorial-modal").modal({
-        fadeDuration: 200
+
     });
 });
