932a18ec2980982ae70fe2623b988095d01715c9
Nahuel <nahuelmath@gmail.com>
2023-07-02 05:31:34 -0400

    feat: add complete tutorial for home page use

--- web_design/static/js/tutorial_modal.js
+++ web_design/static/js/tutorial_modal.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
     $(".show-modal-tutorial").click(function() {
-        const tutorialIntroHtmlContent = `
-        <div id="container-modal-tutorial-icon">
-            <p style="font-size: 18px">Hola bienvenido a Financhef, acá podrás encontrar ayuda para utilizar
-            la aplicacion web número uno de Beauchef:</p>
-            <a class="show-modal-tutorial-secondary" style="cursor: pointer; color: #b91a1d;font-size: 20px; font-weight: 700;">Enseñame!</a>
-        </div>`;
-    
-        $("#tutorial-modal").html(tutorialIntroHtmlContent);
         $("#tutorial-modal").modal({
             fadeDuration: 200
         });
-
-
     });
+
+    $(".show-modal-tutorial-secondary").click(function() {
+        $("#firts-tutorial-modal").modal({
+            fadeDuration: 200
+        })
+    })
+
+    $(".show-modal-tutorial-third").click(function() {
+        $("#second-tutorial-modal").modal({
+            fadeDuration: 200
+        })
+    })
+
+    $(".show-modal-tutorial-fourth").click(function() {
+        $("#third-tutorial-modal").modal({
+            fadeDuration: 200
+        })
+    })
 });
