a23ca4df8613bfb527322063464a13984c2d1c7b
Javier Kauer <kauer.javier01@gmail.com>
2023-07-04 00:09:14 -0400

    feat: 10 new global news

--- web_design/static/js/news_of_the_world.js
+++ web_design/static/js/news_of_the_world.js
@@ -8,7 +8,18 @@ var mensajes = [
     "La cafeta anuncia convenio con McDonald's.",
     "Licorerías cercanas a la facultad al borde la quiebra por falta de fiestas de los viernes.",
     "DCC regala computadores gamer a sus estudiantes gracias al trabajo del grupo 18 del ramo CC4401.",
-    "Venta de chocolates, rosas y peluches aumentan un 1000% gracias a las lluvias."
+    "Venta de chocolates, rosas y peluches aumentan un 1000% gracias a las lluvias.",
+    "BREAKING NEWS: Se viene fiesta masiva el jueves 6 de julio",
+    "Se inaugura un nuevo laboratorio de inteligencia artificial en la facultad de Ingeniería de la FCFM.",
+    "Estudiantes de Ingeniería en Computación ganan competencia de programación a nivel nacional.",
+    "Crean una nueva cátedra de criptografía avanzada en el Departamento de Ciencias de la Computación.",
+    "Aumenta la demanda de pasantías en empresas de tecnología para estudiantes de ingeniería en computación.",
+    "Estudiantes del DCC desarrollan una aplicación móvil para facilitar el proceso de matrícula.",
+    "Se implementa un nuevo programa de mentoría entre estudiantes de ingeniería en computación de primer y último año.",
+    "Investigadores del DCC obtienen fondos para desarrollar un proyecto de realidad virtual aplicada a la educación.",
+    "Se abre una convocatoria para la creación de un centro de emprendimiento tecnológico en la facultad de Ingeniería.",
+    "Estudiantes del DCC participan en una competencia de robótica y obtienen el primer lugar.",
+    "Conferencia sobre inteligencia artificial y aprendizaje automático se lleva a cabo en la FCFM, con destacados ponentes internacionales."
 ];
 
 
